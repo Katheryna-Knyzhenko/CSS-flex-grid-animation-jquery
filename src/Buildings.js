6a2a6887f6ba1c6ea569d5692fedb3709e2aa6c9
Kate <evgen.knyzhenko@gmail.com>
2020-03-31 17:20:00 +0300

    gridBuildings

--- src/Buildings.js
+++ src/Buildings.js
@@ -15,7 +15,20 @@ class Buildings extends Component {
             <div className={styles.buildingsVariants}>
                 <div className={styles.building1}>1</div>
                 <div className={styles.building2}>2</div>
-                <div className={styles.building3}>3</div>
+                <div className={styles.building3}>
+                <div className={styles.home1}>Домик 1</div>
+                    <div className={styles.home2}>Домик 2</div>
+                    <div className={styles.home3}>Домик 3</div>
+                    <div className={styles.home4}>Домик 4</div>
+                    <div className={styles.home5}>Домик 5</div>
+                    <div className={styles.home6}>Домик 6</div>
+                    <div className={styles.home7}>Домик 7</div>
+                    <div className={styles.home8}>Домик 8</div>
+                    <div className={styles.home9}>Домик 9</div>
+                    <div className={styles.home10}>Домик 10</div>
+                    <div className={styles.home11}>Домик 11</div>
+                    <div className={styles.home12}>Домик 12</div>
+                </div>
             </div>
         )
     }
