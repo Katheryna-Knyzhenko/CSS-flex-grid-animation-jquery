2509df9ba2adcce394052d9c8fc42e21ca13a02d
Kate <evgen.knyzhenko@gmail.com>
2020-04-03 15:13:14 +0300

    flatsGrid

--- src/FlatsList/FlatsList.js
+++ src/FlatsList/FlatsList.js
@@ -11,13 +11,16 @@ class FlatsList extends Component {
 
         return (
             <div className={styles.wrapper}>
-                                                <div class="foo">
-	<h1>1</h1>
-</div>
-<div class="bar">
-	<h2>2</h2>
-</div>
-                <div>Список квартир</div>
+                <div className={styles.list}>Список квартир
+                <div className={styles.variants + styles.variant1}>Квартира 1</div>
+                <div className={styles.variants + styles.variant2}>Квартира 2</div>
+                <div className={styles.variants + styles.variant3}>Квартира 3</div>
+                <div className={styles.variants + styles.variant4}>Квартира 4</div>
+                <div className={styles.variants + styles.variant5}>Квартира 5</div>
+                <div className={styles.variants + styles.variant6}>Квартира 6</div>
+                    <div className={styles.variants + styles.variant7}>Квартира 7</div>
+                    <div className={styles.variants + styles.variant8}>Квартира 8</div>
+                </div>
             </div>
         )
     }
