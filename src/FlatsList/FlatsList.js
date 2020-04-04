f6d0a5f7a9f41db4a06ecc3ae1869ebc5377e8e4
Kate <evgen.knyzhenko@gmail.com>
2020-04-04 15:22:53 +0300

    flantListGrid

--- src/FlatsList/FlatsList.js
+++ src/FlatsList/FlatsList.js
@@ -12,14 +12,14 @@ class FlatsList extends Component {
         return (
             <div className={styles.wrapper}>
                 <div className={styles.list}>Список квартир</div>
-                <div className={styles.variants + styles.variant1}>Квартира 1</div>
-                <div className={styles.variants.variant2}>Квартира 2</div>
-                <div className={styles.variants.variant3}>Квартира 3</div>
-                <div className={styles.variants.variant4}>Квартира 4</div>
-                <div className={styles.variants.variant5}>Квартира 5</div>
-                <div className={styles.variants.variant6}>Квартира 6</div>
-                    <div className={styles.variants.variant7}>Квартира 7</div>
-                    <div className={styles.variants.variant8}>Квартира 8</div>
+                <div className={styles.variants + " " + styles.variant1}>Квартира 1</div>
+                <div className={styles.variants + " " + styles.variant2}>Квартира 2</div>
+                <div className={styles.variants + " " + styles.variant3}>Квартира 3</div>
+                <div className={styles.variants + " " +  styles.variant4}>Квартира 4</div>
+                <div className={styles.variants + " " + styles.variant5}>Квартира 5</div>
+                <div className={styles.variants + " " + styles.variant6}>Квартира 6</div>
+                    <div className={styles.variants + " " + styles.variant7}>Квартира 7</div>
+                    <div className={styles.variants + " " + styles.variant8}>Квартира 8</div>
             </div>
         )
     }
