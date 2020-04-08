90c61ea5ee88f577b86bfc7ca5628cee95efcb8b
Kate <evgen.knyzhenko@gmail.com>
2020-04-08 16:11:51 +0300

    parallaxOffer

--- src/Offers/Offers.js
+++ src/Offers/Offers.js
@@ -10,7 +10,26 @@ class Offers extends Component {
 
     render() {
         return (
-            <div className={styles.wrap}> Привет </div>
+            <section>
+            <div className={styles.wrapper}>
+            <div className = {styles.parallax}>
+                 <h1>Welcome!</h1>
+            </div>
+                <div className={styles.wrapText}>
+                <div className = {styles.textContent}>
+                    <p>
+                        текст 1
+                        <br/><br/>
+                    текст 2
+                        <br/><br/>
+                        текст 3
+
+                    </p>
+
+            </div>
+                </div>
+            </div>
+            </section>
         )
     }
 }
