a7c2ce419a2c04cedda6de6397995d72c6950e83
Kate <evgen.knyzhenko@gmail.com>
2020-04-09 17:29:24 +0300

    parallaxOffer

--- src/Offers/Offers.js
+++ src/Offers/Offers.js
@@ -1,5 +1,10 @@
 import React, {Component} from "react";
 import styles from './Offers.module.css';
+import $ from "jquery";
+import imageFlat from "./flatlittle3.jpg";
+
+
+
 
 
 class Offers extends Component {
@@ -7,8 +12,19 @@ class Offers extends Component {
         super();
         this.state = {}
     }
-
+    // componentDidMount() {
+    //     $(window).stellar()
+    // }
+    //
+    // componentWillUnmount() {
+    //     $(window).stellar("destroy");
+    // }
     render() {
+        // $(document).ready(function ($) {
+        //     $(window).stellar();
+        //
+        // })
+
         return (
             <section>
             <div className={styles.wrapper}>
@@ -18,17 +34,21 @@ class Offers extends Component {
                 <div className={styles.wrapText}>
                 <div className = {styles.par_textContent}>
                     <div className={styles.p}>
-                        <div className={styles.text1}> текст 1</div>
-                        <div className={styles.text2}> текст 2</div>
-                        <div className={styles.text3}> текст 3</div>
-                        <div className={styles.text4}> текст 4</div>
+                        <div className={styles.text1} data-stellar-background-ratio="0.5"> текст 1
+                            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                        </div>
+                        <div className={styles.text2} data-stellar-background-ratio="0.9"> текст 2</div>
+                        <div className={styles.text3} data-stellar-background-ratio="0.3"> текст 3</div>
                     </div>
 
             </div>
                 </div>
             </div>
+
             </section>
-        )
+        );
+
     }
+
 }
 export default Offers;
