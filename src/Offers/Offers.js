c397ccb6e836de3abcbf319c21460de4ff20db93
Kate <evgen.knyzhenko@gmail.com>
2020-04-09 17:44:35 +0300

    parallaxOffer

--- src/Offers/Offers.js
+++ src/Offers/Offers.js
@@ -34,11 +34,11 @@ class Offers extends Component {
                 <div className={styles.wrapText}>
                 <div className = {styles.par_textContent}>
                     <div className={styles.p}>
-                        <div className={styles.text1} data-stellar-background-ratio="0.5"> текст 1
+                        <div className={styles.text1} data-stellar-background-ratio="0.5">
                             {/* eslint-disable-next-line jsx-a11y/alt-text */}
                         </div>
-                        <div className={styles.text2} data-stellar-background-ratio="0.9"> текст 2</div>
-                        <div className={styles.text3} data-stellar-background-ratio="0.3"> текст 3</div>
+                        <div className={styles.text2} data-stellar-background-ratio="0.9"> Квартиры</div>
+                        <div className={styles.text3} data-stellar-background-ratio="0.3"> </div>
                     </div>
 
             </div>
