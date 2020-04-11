908d084b25d88be65b587ae3dabdd6470a00541c
Kate <evgen.knyzhenko@gmail.com>
2020-04-11 17:26:01 +0300

    localStorageSideBar

--- src/SideBar/SideBar.js
+++ src/SideBar/SideBar.js
@@ -9,9 +9,14 @@ class SideBar extends Component {
     }
 
     render() {
+        var elementItem = document.getElementById('item');
+        localStorage.setItem(document.getElementById('item'), 'event.target.value');
+        var localValue = localStorage.getItem(document.getElementById('item'));
+        console.log(localValue);
+        localStorage.removeItem(document.getElementById('item'));
 
         return (
-<div>
+          <div>
             <div className={styles.sidebar}>
                 <p> <span className={styles.aboutSite}>Я первый сайдбар</span>
                 </p>
@@ -19,12 +24,14 @@ class SideBar extends Component {
                 <NavLink className={styles.link} to = "/intrOffers">Интересные предложения (Вам сюда!)</NavLink>
                 </p>
                 <form id="form" action="#" method="POST">
-                <textarea id="description" className={styles.textArea} type="text" placeholder="Что Вы ищите?"></textarea>
+                <textarea id="item"  className={styles.textArea} type="text" placeholder="Что Вы ищите?"></textarea>
                     <p>
                 <button id="add" type="submit" className={styles.but}>Add</button>
                     </p>
                     <button className={styles.but} id="clear">Clear All</button>
                 </form>
+                <h2>Items</h2>
+                 <ul id="todos"></ul>
             </div>
             <div className={styles.sidebar2}>
                 <p> <span className={styles.aboutSite}>Я второй сайдбар</span>
@@ -38,5 +45,6 @@ class SideBar extends Component {
         )
     }
 }
+
 
 export default SideBar;
